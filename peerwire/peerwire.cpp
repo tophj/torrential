4cb9ecf67de475c2e44e459bebb611a167b5ff9c
Chris Jones <Toph@Toph.local>
2013-10-07 16:34:19 -0400

    Added peerwire folders

---
+++ peerwire/peerwire.cpp
@@ -0,0 +1,9 @@
+// Basically, given a user look at the tracker to get a list of peers.
+// Then, iterate through that list of peers and look to see if they have the 
+// specific piece that tbe user needs.
+// If they do, then start a new thread that connects / handshakes through FTP to
+// download that piece. 
+// Keep looking at the pieces that peer has available until you either hit the limit of 
+// pieces you can download simultaneously, or hit the download limit.
+
+
