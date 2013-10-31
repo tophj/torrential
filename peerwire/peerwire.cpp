18236872240ac7aa52849a3b1ab6f2a505708b71
Chris Jones <tophj@vt.edu>
2013-10-30 21:29:02 -0400

    Hey

--- peerwire/peerwire.cpp
+++ peerwire/peerwire.cpp
@@ -18,8 +18,8 @@ TorrentPeerwireProtocol::TorrentPeerwireProtocol(uint8_t info_hash[20],struct th
 													  PeerList & pList){
 
 	
-	printf("Launching Peerwire...");
-	printf("Downloading...");
+	printf("Launching Peerwire...\n");
+	printf("Searching through peers...\n");
 	//const std::string stringtest = "nope";
 	//uint8_t peer_id[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17, 18, 19, 20};
 	std::vector<Peer> peerList;
