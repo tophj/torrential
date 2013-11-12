37960090bc7c9ed10d7929d8ba6b609015af7423
egaebel <egaebel@vt.edu>
2013-11-12 06:54:14 -0500

    CORRECT peer changes...wtf

--- Peer.h
+++ Peer.h
@@ -10,13 +10,41 @@ class Peer {
 
 	private:
 		//~Data Fields---------------------------------------------------
+		//The id of the peer.
 		//std::string id;
 
+		//The IP address for the Peer.
 		std::string ip;
 
+		//The port number for the Peer.
 		uint16_t portNumber;
 
 	public:
+		//~Choking Fields-------------------------------------------
+		/* Whether or not the remote peer has choked this client. When a peer chokes the client, 
+		   it is a notification that no requests will be answered until the client is unchoked. 
+		   The client should not attempt to send requests for blocks, 
+		   and it should consider all pending (unanswered) requests to be discarded by the remote peer. */
+		/* Client is choking the peer */
+		bool amChoking = false;
+
+		/* Peer is */
+		bool peerChoking = false;
+
+
+		//~Interested Fields-------------------------------------------
+		/* Whether or not the remote peer is interested in something this client has to offer. 
+		This is a notification that the remote peer will begin requesting blocks when the client unchokes them.
+		Note: this also implies that the client will also need to keep track of whether or not it is interested in the remote peer, 
+		and if it has the remote peer choked or unchoked. So, the real list looks something like this: */
+		
+		/* Client is interested in the peer. */
+		bool amInterested = false;
+
+		/* Peer is interested in the client. */
+		bool peerInterested = false;
+
+
 		//~Constructors/Destructor-------------------------------------
 		/* Creates a new Peer with the specified ID, 
 		   sets the IP string, and
